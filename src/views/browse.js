6935c4e2f97402f65a81cb49fc4877df165213f9
Luke Jackson <lukejacksonn@gmail.com>
2017-06-05 15:06:47 +0100

    Switches browse to h functions

--- src/views/browse.js
+++ src/views/browse.js
@@ -1,39 +1,70 @@
 import { h } from 'hyperapp'
 import { timeSince } from '../helpers/time'
-export default ({user, browse, a}) =>
-  <browse->
-    <a href={browse.url} target='_blank'>
-      <screenshot-
-        onclick={() => a.browse.view(browse.key)}
-        style={{ backgroundImage: `url(${browse.image})` }}
-      ></screenshot->
-    </a>
-    <meta->
-      <a href={'/'+browse.browser}>
-        <img
-          class='avatar'
-          src={`https://graph.facebook.com/${browse.browser}/picture?type=square`}
-        />
-      </a>
-      <col->
-        <title->{browse.title}</title->
-        <row->
-          <browsers->{ Object.keys(browse.browsers || {})
-          .filter(id => id !== browse.browser)
-          .map(id =>
-            <a href={'/'+id}>
-              <img src={`https://graph.facebook.com/${id}/picture?type=square`} />
-            </a>
-          )}</browsers->
-          <time->{timeSince(browse.published)}</time->
-        </row->
-      </col->
-      { user && user.fbid === browse.browser ?
-        <button class='delete' onclick={() => a.browse.delete(browse.key)}>
-          <svg viewBox='0 0 32 32' fill='none' stroke='currentcolor' stroke-linecap='round' stroke-linejoin='round' stroke-width='6.25%' xmlns='http://www.w3.org/2000/svg'>
-            <path d='M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6'></path>
-          </svg>
-        </button> : ''
-      }
-    </meta->
-  </browse->
+
+const Screenshot = ({ url, image, key, a }) =>
+  h('a', { href: url, target: '_blank' },[
+    h('screenshot-', {
+      style: { backgroundImage: `url(${image})` },
+      onclick: _ => a.browse.view(key),
+    })
+  ])
+
+const FacebookAvatar = id =>
+  h('a', { href: '/' + id }, [
+    h('img', {
+      class: 'avatar',
+      src: `https://graph.facebook.com/${id}/picture?type=square`,
+    })
+  ])
+
+const BrowsersAvatarList = ({
+    browser,
+    browsers = {},
+  }) =>
+  h('browsers-', null, Object.keys(browsers)
+    .filter(id => id !== browser)
+    .map(FacebookAvatar)
+  )
+
+const DeleteBrowseButton = ({
+    user,
+    browser,
+    key,
+    a
+  }) =>
+  user !== browser ? null :
+  h('button', {
+    class: 'delete',
+    onclick: _ => a.browse.delete(key),
+  },[
+    <svg viewBox='0 0 32 32' fill='none' stroke='currentcolor' stroke-linecap='round' stroke-linejoin='round' stroke-width='6.25%' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6'></path>
+    </svg>
+  ])
+
+const Row = xs => h('row-', {}, xs)
+const Col = xs => h('col-', {}, xs)
+const Meta = xs => h('meta-', {}, xs)
+const Browse = xs => h('browse-', {}, xs)
+const Title = text => h('title-', {}, text)
+const TimeSince = time => h('time-', {}, timeSince(time))
+
+export default ({
+  user = {},
+  a = {},
+  browse = {},
+}) =>
+Browse([
+  Screenshot({ ...browse, a }),
+  Meta([
+    FacebookAvatar(browse.browser),
+    Col([
+      Title(browse.title),
+      Row([
+        BrowsersAvatarList({ ...browse }),
+        TimeSince(browse.published),
+      ]),
+    ]),
+    DeleteBrowseButton({ ...browse, user: user.fbid, a }),
+  ]),
+])
